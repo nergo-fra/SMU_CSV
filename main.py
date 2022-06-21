39e7ba8b3529f2233f60bbdebf549d3874f96ca4
Margaux Forner <margaux.forner@etu.emse.fr>
2022-06-21 18:25:13 +0100

    Updated main : function settings commands added comments to follow exec progress

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def _read_csv(name):
     return df
 
 def _settings_commands_SMU(inst, parameters, V_list, wait = True):
+    print("SMU settings in progress : 0% \n")
     # parameters format (list of strings, units of seconds, amps, volts):
     # [(0) S1 compliance current,(1) time per point,(2) points per sweep,(3) acquisition time,
     #  (4) premeasurement voltage hold time]
@@ -52,29 +53,35 @@ def _settings_commands_SMU(inst, parameters, V_list, wait = True):
     inst.write(":sour2:func:lev:imm 0")
     inst.write(":sour1:volt:prot 2")
 
+    print("SMU settings in progress : 20% \n Turning outputs on ... \n")
     # Turning outputs on
     inst.write(":outp1 on")
     inst.write(":outp2 on")
+    print("Outputs on \n")
 
     # If wait is needed (if the SMU is not on yet)
     if wait:
         time.sleep(parameters[4])
 
+    print("SMU settings in progress : 30% \n")
     # Sets the measurement list of voltages (channel 1)
     inst.write(":sour1:volt:mode list")
     inst.write(":sour1:list:volt " + ch1_list)
 
+    print("SMU settings in progress : 40% \n")
     # Sense settings
     inst.write(":sens1:func \"volt\"")
     inst.write(":sens2:func \"volt\"")
     inst.write(":sens2:volt:rang:auto on")
 
+    print("SMU settings in progress : 50% \n")
     # Measurement wait time set to OFF
     inst.write(":sens1:wait off")
     inst.write(":sour1:wait off")
     inst.write(":sens2:wait off")
     # sour2 not touched because we don't really care
 
+    print("SMU settings in progress : 55% \n")
     # Set trigger source to the same mode
     inst.write(":trig1:sour tim")
     inst.write("trig1:tim " + parameters[1])
@@ -89,16 +96,20 @@ def _settings_commands_SMU(inst, parameters, V_list, wait = True):
     inst.write("trig2:tran:coun " + parameters[2])
     inst.write(":trig2:tran:del def")
 
+    print("SMU settings in progress : 80% \n")
     # Measurement interval is set to the same value
     inst.write(":sens1:curr:aper " + parameters[3])
     inst.write(":sens2:volt:aper " + parameters[3])
 
+    print("SMU settings in progress : 95% \n")
     # Output formatting
     inst.write(":form:elem:sens volt,curr,time")
 
+    print("SMU settings in progress : 100% \n Running measurements ...\n")
     # Running measurements
     inst.write(":init (@1,2)")
 
+    print("Measurements done! \n Fetching data \n")
     # Fetching data - there is a more elegant way to do that
     data_LED_0 = inst.query(":fetc:arr:time? (@1)")
     data_LED_1 = inst.query(":fetc:arr:curr? (@1)")
@@ -106,19 +117,16 @@ def _settings_commands_SMU(inst, parameters, V_list, wait = True):
     data_detect_0 = inst.query(":fetc:arr:time? (@2)")
     data_detect_1 = inst.query(":fetc:arr:volt? (@2)")
 
+    print("Data export... \n"))
     # Transforming data from list to array
     data_led_np_0 = np.asarray([float(i) for i in data_LED_0.split(',')])
     data_led_np_1 = np.asarray([float(i) for i in data_LED_1.split(',')])
     data_led_np_2 = np.asarray([float(i) for i in data_LED_2.split(',')])
     data_detect_np_0 = np.asarray([float(i) for i in data_detect_0.split(',')])
     data_detect_np_1 = np.asarray([float(i) for i in data_detect_1.split(',')])
-
+    print("Done\n")
     return data_detect_np_0, data_detect_np_1, data_led_np_0, data_led_np_1, data_led_np_2
 
-
-
-def _run_measurements():
-    print("done")
 
 # MAIN
 
