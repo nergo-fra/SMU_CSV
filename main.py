cfe7bbf0aa67503b04c7e5deb146bdaf52388398
nergo-fra <107041866+nergo-fra@users.noreply.github.com>
2022-06-22 18:25:49 +0100

    Add files via upload
    stable main

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ plt.style.use('dark_background')
 
 # PARAMETERS TO MODIFY
 
-I_comp = 0.0007 # current compliance S1
-time_per_point = 3.3333e-05  # Minimum value of 2e-5 #use time between measures from csv
+I_comp = 2 # current compliance S1
+time_per_point = 3e-3  # Minimum value of 2e-5 #use time between measures from csv
 time_before_meas = 2 #if SMU is not on, we give it time to initialize
 
 # FUNCTION
@@ -38,50 +38,51 @@ time_before_meas = 2 #if SMU is not on, we give it time to initialize
 def _read_csv(name):
     df = pd.read_csv(name, names=["Volt"], index_col=0, skiprows=1, low_memory=False)
     df.astype('float').dtypes
+    #df_2 = df.truncate(after='1970-01-01 00:00:00.001300000')
     return df
 
 def _settings_commands_SMU(inst, parameters, V_list, wait = True):
-    print("SMU settings in progress : 0% \n")
+    print("SMU settings in progress : 0%")
     # parameters format (list of strings, units of seconds, amps, volts):
     # [(0) S1 compliance current,(1) time per point,(2) points per sweep,(3) acquisition time,
     #  (4) premeasurement voltage hold time]
     # Source settings before turn on - the second source is set as shown by carefulness, we wouldn't want to inject a current or voltage in the detector
-    inst.write(":sour1:func:mode volt")
-    inst.write(":sour1:volt:lev:imm" + V_list.split(',')[0])
-    inst.write(":sour1:volt:prot " + parameters[0])
-    inst.write(":sour2:func:mode curr")
-    inst.write(":sour2:func:lev:imm 0")
-    inst.write(":sour1:volt:prot 2")
+    inst.write(":sour1:volt:lev:imm " + V_list.split(',')[0])
+    inst.write(":sens1:curr:prot " + parameters[0])
+    #inst.write(":sour2:func:mode curr")
+    #inst.write(":sour2:func:lev:imm 0")
 
-    print("SMU settings in progress : 20% \n Turning outputs on ... \n")
+    print("SMU settings in progress : 20% \nTurning outputs on ...")
     # Turning outputs on
     inst.write(":outp1 on")
     inst.write(":outp2 on")
-    print("Outputs on \n")
+    print("Outputs on")
 
     # If wait is needed (if the SMU is not on yet)
     if wait:
         time.sleep(parameters[4])
 
-    print("SMU settings in progress : 30% \n")
+    print("SMU settings in progress : 30%")
     # Sets the measurement list of voltages (channel 1)
+    inst.write(":sour1:func:mode volt")
     inst.write(":sour1:volt:mode list")
     inst.write(":sour1:list:volt " + V_list)
 
-    print("SMU settings in progress : 40% \n")
+    print("SMU settings in progress : 40%")
     # Sense settings
     inst.write(":sens1:func \"volt\"")
     inst.write(":sens2:func \"volt\"")
     inst.write(":sens2:volt:rang:auto on")
+    inst.write(":sens1:volt:rang:auto on")
 
-    print("SMU settings in progress : 50% \n")
+    print("SMU settings in progress : 50%")
     # Measurement wait time set to OFF
     inst.write(":sens1:wait off")
     inst.write(":sour1:wait off")
     inst.write(":sens2:wait off")
     # sour2 not touched because we don't really care
 
-    print("SMU settings in progress : 55% \n")
+    print("SMU settings in progress : 55%")
     # Set trigger source to the same mode
     inst.write(":trig1:sour tim")
     inst.write("trig1:tim " + parameters[1])
@@ -96,41 +97,43 @@ def _settings_commands_SMU(inst, parameters, V_list, wait = True):
     inst.write("trig2:tran:coun " + parameters[2])
     inst.write(":trig2:tran:del def")
 
-    print("SMU settings in progress : 80% \n")
+    print("SMU settings in progress : 80%")
     # Measurement interval is set to the same value
-    inst.write(":sens1:curr:aper " + parameters[3])
-    inst.write(":sens2:volt:aper " + parameters[3])
+    inst.write(":sens1:curr:aper:auto on")
+    inst.write(":sens2:volt:aper " + parameters[5])
 
-    print("SMU settings in progress : 95% \n")
+    print("SMU settings in progress : 95%")
     # Output formatting
     inst.write(":form:elem:sens volt,curr,time")
 
-    print("SMU settings in progress : 100% \n Running measurements ...\n")
+    print("SMU settings in progress : 100% \nRunning measurements ...")
     # Running measurements
     inst.write(":init (@1,2)")
 
-    print("Measurements done! \n Fetching data \n")
-    data_raw = np.empty((len(V_list.split(",")), 5), float)
+    print("Measurements in progress : Fetching data...")
     # Fetching data - there is a more elegant way to do that using read
-    data_raw[0] = inst.query(":fetc:arr:time? (@1)")
-    data_raw[1] = inst.query(":fetc:arr:curr? (@1)")
-    data_raw[2] = inst.query(":fetc:arr:volt? (@1)")
-    data_raw[3] = inst.query(":fetc:arr:time? (@2)")
-    data_raw[4] = inst.query(":fetc:arr:volt? (@2)")
+    data_raw_0 = inst.query(":fetc:arr:time? (@1)")
+    data_raw_1 = inst.query(":fetc:arr:curr? (@1)")
+    data_raw_2 = inst.query(":fetc:arr:volt? (@1)")
+    data_raw_3 = inst.query(":fetc:arr:time? (@2)")
+    data_raw_4 = inst.query(":fetc:arr:volt? (@2)")
 
-    print("Data export... \n")
+    print("Data export...")
     # Transforming data from list to array
-    data = np.empty((len(V_list.split(",")), 5), float)
-    for i in range(5):
-        data[i] = np.asarray([float(i) for i in data_raw[i].split(',')])
-    print("Done\n")
-    return data
+    data_led_np_0 = np.asarray([float(i) for i in data_raw_0.split(',')])
+    data_led_np_1 = np.asarray([float(i) for i in data_raw_1.split(',')])
+    data_led_np_2 = np.asarray([float(i) for i in data_raw_2.split(',')])
+    data_detect_np_0 = np.asarray([float(i) for i in data_raw_3.split(',')])
+    data_detect_np_1 = np.asarray([float(i) for i in data_raw_4.split(',')])
+    print("Done")
+    return data_detect_np_0, data_detect_np_1, data_led_np_0, data_led_np_1, data_led_np_2
+
 
 def _generate_sweep_from_pd(df):
     sweep = ""
-    for i in range(0, len(df["Volt"])):
-        sweep += "{:.6E}".format(df["Volt"][i], 6) + ","
-    sweep += "{:.5E}".format(df["Volt"][len(df["Volt"])], 5)
+    for i in range(0, len(df["Volt"]) - 1):
+        sweep += "{:.3E}".format(df["Volt"][i], 3) + ","
+    sweep += "{:.3E}".format(df["Volt"][len(df["Volt"]) - 1], 3)
     return sweep
 
 # MAIN
@@ -147,21 +150,22 @@ if __name__ == '__main__':
     assert (inst.query("*IDN?") == "Keysight Technologies,B2902A,MY51143745,3.4.2011.5100\n"), \
         print("Houston, we have a problem")
     inst.timeout = 500 * 1e5  # to change, depends on longest measurement. Not in the readme but pretty obvious
-    df = _read_csv("spinal_normalized1.csv")
+    df = _read_csv("spinal_normalized_1_4_trunc.csv")
     V_list = _generate_sweep_from_pd(df)
 
     # parameters format (list of strings, units of seconds, amps, volts):
     # [(0) S1 compliance current,(1) time per point,(2) points per sweep,(3) acquisition time,
-    #  (4) premeasurement voltage hold time]
+    #  (4) premeasurement voltage hold time, (5) measurement delay]
     parameters = []
     parameters.append("{:.0E}".format(I_comp))
-    parameters.append("{:.0f}".format(len(V_list.split(','))))
+    parameters.append("{:.1E}".format((time_per_point)))
+    parameters.append("{:.1f}".format(len(V_list.split(','))))
     parameters.append("{:.1E}".format(time_per_point / 2))
-    parameters.append("{:.1E}".format((time_per_point / 2)))
     parameters.append(time_before_meas)
+    parameters.append("{:.1E}".format((time_per_point / 2)))
 
     # Create empty array to store output data
-    transfer = np.empty((len(V_list.split(",")), 8), float)
+    transfer = np.empty((len(V_list.split(",")), 5), float)
     transfer = _settings_commands_SMU(inst, parameters, V_list, False)
 
     # Plotting
